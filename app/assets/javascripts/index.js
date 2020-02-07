2652c23786379e7fdf955d27dc7c0d547d9b1bb4
Tanaka Koyo <tanakakoyo1999@icloud.com>
2020-02-07 12:44:55 +0900

    出題設定
    過去問道場画面の実装。
    出題設定追加。

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,5 +1,5 @@
-/*Dropdown Menu*/
 $(function(){
+    // ヘッダー試験言語プルダウン表示
     $('.dropdown').click(function () {
         $(this).attr('tabindex', 1).focus();
         $(this).toggleClass('active');
@@ -13,12 +13,22 @@ $(function(){
         $(this).parents('.dropdown').find('span').text($(this).text());
         $(this).parents('.dropdown').find('input').attr('value', $(this).attr('id'));
     });
-    /*End Dropdown Menu*/
-    
-    
     $('.dropdown-menu li').click(function () {
     var input = '<strong>' + $(this).parents('.dropdown').find('input').val() + '</strong>',
       msg = '<span class="msg">Hidden input value: ';
     $('.msg').html(msg + input + '</span>');
-    }); 
+    });
+    
+    // 出題設定「ON/OFF」トグル
+    toggle = ['input[type="checkbox"]#grade','input[type="checkbox"]#year','input[type="checkbox"]#field','input[type="checkbox"]#option']
+    $(toggle[0]+","+toggle[1]+","+toggle[2]+","+toggle[3]).change(function() {
+        toggle_id = this.id
+        if ($(this).is(':checked')) {
+            //全てオフにする
+            $('#practice_left_setting_'+toggle_id+' input[name="'+toggle_id+'"]').prop('checked', true);
+        } else {
+            //全てオフにする
+            $('#practice_left_setting_'+toggle_id+' input[name="'+toggle_id+'"]').prop('checked', false);
+        }
+    });
 })
