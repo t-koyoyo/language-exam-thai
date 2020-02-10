d13b786c10c72fde5f9750c7e466ad6b9967fd5c
Tanaka Koyo <tanakakoyo1999@icloud.com>
2020-02-11 03:01:09 +0900

    過去問道場、おすすめサイト・動画画面
    一通りの過去問道場レイアウト完了。
    おすすめサイト・動画新規作成。

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'question#index'
   get "/question", to: "question#setting"
   post "/question", to: "question#practice"
+  get "/recommend", to: "recommend#index"
 
 end
